b7a0441ed3213eb9fa41e9c81d47271dd287842e
Juan Huttemann <juanfhuttemann@gmail.com>
2018-07-09 12:51:33 -0400

    fix cursor color

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var chokidar = require('chokidar');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const fs = require('fs');
 
 
 app.get('/', function (req, res) {
@@ -21,8 +22,11 @@ http.listen(3000, function () {
 });
 
 io.on('connection', function (socket) {
+  fs.readFile('sample-log-file.txt', (err, data) => {
+    if (err) throw err;
+      socket.emit('log', data.toString().split("\n").join('<br>'));
+  });
     chokidar.watch('sample-log-file.txt', { ignored: /(^|[\/\\])\../ }).on('change', (event, path) => {
-        const fs = require('fs');
         fs.readFile('sample-log-file.txt', (err, data) => {
           if (err) throw err;
             socket.emit('log', data.toString().split("\n").join('<br>'));
