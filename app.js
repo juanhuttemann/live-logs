51dc04a2014b93d7dbde575e992652c16da974d8
Juan Huttemann <juanfhuttemann@gmail.com>
2018-07-16 07:37:36 -0400

    add toggle on/off logs

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
-var chokidar = require('chokidar');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-const fs = require('fs');
+const chokidar = require('chokidar'),
+  app = require('express')(),
+  http = require('http').Server(app),
+  io = require('socket.io')(http),
+  fs = require('fs'),
+  PORT = 3000,
+  LOG_FILE = 'sample-log-file.txt'
 
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/style.css', function (req, res) {
-    res.sendFile(__dirname + '/css/style.css');
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname + '/css/style.css');
 });
 
-app.get('/typer.js', function (req, res) {
-    res.sendFile(__dirname + '/js/typer.js');
+app.get('/toggle.css', function(req, res) {
+  res.sendFile(__dirname + '/css/toggle.css');
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+app.get('/typer.js', function(req, res) {
+  res.sendFile(__dirname + '/js/typer.js');
 });
 
-io.on('connection', function (socket) {
+http.listen(PORT, function() {
+  console.log(`listening on PORT: ${PORT}`);
+});
+
+function fileReader(socket) {
   fs.readFile('sample-log-file.txt', (err, data) => {
     if (err) throw err;
-      socket.emit('log', data.toString().split("\n").join('<br>'));
+    socket.emit('log', data.toString().split("\n").join('<br>'));
   });
-    chokidar.watch('sample-log-file.txt', { ignored: /(^|[\/\\])\../ }).on('change', (event, path) => {
-        fs.readFile('sample-log-file.txt', (err, data) => {
-          if (err) throw err;
-            socket.emit('log', data.toString().split("\n").join('<br>'));
-        });
+}
 
+io.on('connection', function(socket) {
+  fileReader(socket)
+  let watcher = chokidar.watch().on('change', () => {
+    fileReader(socket)
+  });
+
+  socket.on('readLogs', function(data) {
+    fileReader(socket)
+    watcher = chokidar.watch('sample-log-file.txt').on('change', () => {
+      fileReader(socket)
     });
+    console.log("WATCHER STARTED");
+  });
+
+  socket.on('stopLogs', function(data) {
+    watcher.close();
+    console.log("WATCHER STOPED");
+  });
 });
